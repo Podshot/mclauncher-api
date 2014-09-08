21fff61249b571a2d58ef2a88bfaffa2fcb50d60
Podshot <ben.gothard3@gmail.com>
2014-09-08 09:57:14 -0400

    MCLauncher additions (Slow development is slow)

--- mclauncher/common/MCLauncher.py
+++ mclauncher/common/MCLauncher.py
@@ -5,14 +5,18 @@ import os
 import sys
 
 class MCLauncher(object):
-    '''
-    classdocs
-    '''
-
 
     def __init__(self, version):
         self.version = version
+        self.getVersionJSON()
     
     def getJar(self):
         urllib.urlretrieve(self.version.getJarURL(), self.version.getJarName())
+        
+    def downloadResources(self):
+        return
+    
+    def getVersionJSON(self):
+        self.version.getVersionJSON()
+        
         
