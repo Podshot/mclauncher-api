973daa2ecc5f932abdce063b0bf3d09b34bd0ab9
Podshot <ben.gothard3@gmail.com>
2014-09-09 11:40:39 -0400

    __init__.py setup, more Platform.py work

---
+++ mclauncher/__init__.py
@@ -0,0 +1,2 @@
+from mclauncher.common import __init__ as mccommon
+from mclauncher.version import __init__ as mcversion
--- mclauncher/common/Platform.py
+++ mclauncher/common/Platform.py
@@ -1,4 +1,14 @@
 from sys import platform as _platform
+import os
+import Windows
+import Macintosh
 
 def getCurrentPlatform():
-    return
+    if _platform == "win32":
+        return Windows.getDisplayName()
+    if _platform == "darwin":
+        return Macintosh.getDisplayName()
+    return None
+
+def getWorkingDirectory():
+    return os.getcwd()
