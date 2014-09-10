47f0fac93bd6bfe2315fe97e2e94bd3cf7381e0f
Podshot <ben.gothard3@gmail.com>
2014-09-09 22:27:16 -0400

    Updated MCDowloadVersionList

--- mclauncher/version/MCDownloadVersionList.py
+++ mclauncher/version/MCDownloadVersionList.py
@@ -9,21 +9,26 @@ class MCDownloadVersionList(object):
     
     def __init__(self):
         self.startDownload()
-        print 'ready'
+        return None
     
     def startDownload(self):
         jsonString = urllib2.urlopen("http://s3.amazonaws.com/Minecraft.Download/versions/versions.json").read()
         versionJSON = json.loads(jsonString)
         for mcVersion in versionJSON["versions"]:
-            print mcVersion
             verJSON = json.loads(urllib2.urlopen('http://s3.amazonaws.com/Minecraft.Download/versions/' + mcVersion['id'] + '/' + mcVersion['id'] + '.json').read())
             ver = Version(mcVersion["id"], verJSON)
             self.versionDictionary[mcVersion["id"]] = ver
         
             print self.versionDictionary[mcVersion["id"]].getID()
+        return
         
     def getVersions(self):
         return self.versionDictionary
+
+'''
+Prevents a double launch bug in PyDev, not to be used in API
+'''
+def getVersionList(): 
+    return MCDownloadVersionList().getVersions()
     
-    
-MCDownloadVersionList()
+getVersionList()
