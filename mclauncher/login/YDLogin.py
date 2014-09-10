f51f6534d8701689836e1a298a4654c88da76c8c
Podshot <ben.gothard3@gmail.com>
2014-09-09 23:32:58 -0400

    Created utils package and split up YDLogin

--- mclauncher/login/YDLogin.py
+++ mclauncher/login/YDLogin.py
@@ -1,5 +1,11 @@
 import json
+import requests
+import httplib
 
+headers = {
+    'User-Agent': 'mclauncher',
+    'Content-Type': "application/json"
+    }
 class YDLoginSessionID(object):
     
     def __init__(self, accessToken, clientToken):
@@ -7,7 +13,22 @@ class YDLoginSessionID(object):
         sessionDict["clientToken"] = clientToken
         sessionDict["accessToken"] = accessToken
         sessionDict["selectedProfile"] = None
-        print sessionDict
+        # A Python None object is automatically converted to a null object in JSON
+        sessionJSON = json.dumps(sessionDict)
+        #response = requests.post("https://authserver.mojang.com/refresh", data=sessionJSON, allow_redirects=True)
+        conn = httplib.HTTPSConnection("authserver.mojang.com")
+        conn.request("POST", "/refresh", sessionJSON, headers)
+        res = conn.getresponse()
+        print "Status: " + str(res.status)
+        print "Reason: " + str(res.reason)
+        print "Response: " + res.read()
+
+        
+        
+class YDPasswordLogin(object):
+    
+    def __init__(self):
+        return
         
         
 YDLoginSessionID("aT", "cT")
---
+++ mclauncher/utils/HTTPUtil.py
@@ -0,0 +1,10 @@
+import urllib
+import urllib2
+import httplib
+ 
+class HTTPGet(object):
+
+
+    def __init__(self, url):
+        return
+        
